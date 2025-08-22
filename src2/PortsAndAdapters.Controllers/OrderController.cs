b4092b91c2279fd71e6126f0e6d4d07ecc469a31
Artem Agafonov <agart@mail.ru>
2025-08-22 22:00:44 +0300

    Extract Controllers project.

--- src2/PortsAndAdapters.Controllers/OrderController.cs
+++ src2/PortsAndAdapters.Controllers/OrderController.cs
@@ -1,8 +1,8 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PortsAndAdapters.Rest.Interfaces;
+using System.Diagnostics.CodeAnalysis;
 
-namespace PortsAndAdapters.Api.Controllers;
+namespace PortsAndAdapters.Controllers;
 
 /// <summary>
 /// API. Order controller.
